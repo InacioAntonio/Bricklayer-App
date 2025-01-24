63cb606be18d73760efee5f09fa62659d496993b
InacioAntonio <inaciodu@outlook.com>
2025-01-23 21:37:33 -0300

    Implementando o auth service

--- lib/core/configure_providers.dart
+++ lib/core/configure_providers.dart
@@ -1,6 +1,7 @@
-import 'package:bricklayer_app/services/realtime_service.dart';
+import 'package:bricklayer_app/services/obra_service.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:bricklayer_app/services/auth_service.dart';
 
 class ConfigureProviders {
   late final List<SingleChildWidget> providers;
@@ -11,6 +12,7 @@ class ConfigureProviders {
 
     return ConfigureProviders(providers: [
       Provider<RealtimeService>.value(value: realtime_service),
+      Provider<AuthService>.value(value: AuthService()),
       //Provider<NotificationService>.value(value: notification_service),
     ]);
   }
